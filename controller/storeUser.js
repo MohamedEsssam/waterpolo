1b3da902f4a8f91531d4de6405b2f450e06d9bc3
MohamedEsssam <mohamedessam994@hotmail.com>
2019-08-12 01:09:22 +0200

    handel exist username

--- controller/storeUser.js
+++ controller/storeUser.js
@@ -1,11 +1,18 @@
 const player = require('../database/models/player')
 
 module.exports = (req, res)=>{
-    player.create(req.body, (err, user)=>{
-        if(err){
-            console.log(err)
+    player.countDocuments({username: req.body.username},(err, count)=>{
+        if(count){
+            console.log('username exist')
             res.redirect('/auth/register')
+        }else{
+            player.create(req.body, (err, user)=>{
+                if(err){
+                    console.log(err)
+                    res.redirect('/auth/register')
+                }
+                res.redirect('/auth/login')
+            })
         }
-        res.redirect('/auth/login')
     })
 }
