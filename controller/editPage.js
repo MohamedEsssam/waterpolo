455a90f5fb6c7428b401e3247a77d91b0e830125
MohamedEsssam <mohamedessam994@hotmail.com>
2019-08-17 17:01:58 +0200

    handle all edit case

--- controller/editPage.js
+++ controller/editPage.js
@@ -1,3 +1,7 @@
 module.exports = (req, res)=>{
-    res.render('edit')
+    res.render('edit', {
+        emptyInfo : req.flash('emptyInfo'),
+        exists : req.flash('userExist'),
+        data : req.flash('data')[0] 
+    })
 }
--- controller/editUser.js
+++ controller/editUser.js
@@ -6,9 +6,23 @@ module.exports = (req, res)=>{
         req.body.username = req.session.username
     }
     if(req.body.fname == '' || req.body.lname == ''){
+        req.flash('emptyInfo', 'you must provide firstname and last name')
        return res.redirect('/auth/edit')
     }
-    player.findByIdAndUpdate(req.session.userId, req.body, (err, user)=>{
-        res.redirect('/auth/Profile')
+    if(req.body.username == req.session.username){
+        player.findByIdAndUpdate(req.session.userId, req.body, (err, user)=>{
+        })
+               return res.redirect('/auth/Profile')
+    }
+    player.findOne({username : req.body.username}, (err, user)=>{
+        if(user){
+            req.flash('userExist', `${req.body.username} is exist change username`)
+            req.flash('data', req.body)
+            return res.redirect('/auth/edit')
+        }else{
+            player.findByIdAndUpdate(req.session.userId, req.body, (err, user)=>{
+                res.redirect('/auth/Profile')
+            })
+        }
     })
 }
